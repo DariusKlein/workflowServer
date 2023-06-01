514ee0c09c88359b6a7d0a8281c1908d537fb12c
Darius <darius.klein@student.hu.nl>
2023-06-01 13:30:59 +0200

    prisma schema update + event log

---
+++ routes/api.js
@@ -0,0 +1,12 @@
+var express = require('express');
+const {events} = require("../services/query's");
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send('Api');
+});
+router.post('/events', async function (req, res, next) {
+    res.send(await events("test", "placeholder1"));
+});
+module.exports = router;
---
+++ services/query's.js
@@ -0,0 +1,16 @@
+const {PrismaClient} = require("@prisma/client");
+
+const prisma = new PrismaClient()
+
+
+async function events(name, type) {
+    if (!name || !type) return "test"
+        return await prisma.Event.create({
+            data: {
+                name: name,
+                type: type,
+            },
+        })
+}
+
+module.exports = {events}
